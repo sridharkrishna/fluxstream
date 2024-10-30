67904d3084bf7bd44624b26246c587700e3eeb3c
Sridhar Ananthakrishnan <sridhar@kriyaetive.com>
2024-10-31 00:19:07 +0530

    basic tcp server

--- src/main.rs
+++ src/main.rs
@@ -16,41 +16,61 @@
 //
 // Author: Sridhar Ananthakrishnan <itsmycodehub@gmail.com>
 
-use hyper::{Body, Request, Response, Server};
-use hyper::service::{make_service_fn, service_fn};
-use std::convert::Infallible;
-use tokio::time::{self, Duration};
-use futures::Stream;
-use std::pin::Pin;
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::thread;
 
-type BoxedStream = Pin<Box<dyn Stream<Item = Result<Body, hyper::Error>> + Send>>;
+/// Handles each incoming connection.
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 512];  // Buffer to store incoming data
 
-async fn handle_request(_req: Request<Body>) -> Result<Response<BoxedStream>, Infallible> {
-    // Simulating a video/audio stream by sending chunks of data over time.
-    let stream = async_stream::try_stream! {
-        for i in 0..10 {
-            yield Body::from(format!("Chunk #{}\n", i));
-            time::sleep(Duration::from_secs(1)).await;
+    loop {
+        // Read data from the stream
+        match stream.read(&mut buffer) {
+            Ok(0) => {
+                // If read returns 0, it means the connection was closed
+                println!("Connection closed by client.");
+                break;
+            },
+            Ok(bytes_read) => {
+                println!("Received: {}", String::from_utf8_lossy(&buffer[..bytes_read]));
+
+                // Echo the data back to the client
+                if let Err(e) = stream.write(&buffer[..bytes_read]) {
+                    eprintln!("Failed to send response: {}", e);
+                    break;
+                }
+            },
+            Err(e) => {
+                eprintln!("Failed to read from connection: {}", e);
+                break;
+            }
         }
-    };
-
-    Ok(Response::new(Box::pin(stream) as BoxedStream))
-}
-
-#[tokio::main]
-async fn main() {
-    let addr = ([127, 0, 0, 1], 3000).into();
-
-    let make_svc = make_service_fn(|_conn| {
-        async { Ok::<_, Infallible>(service_fn(handle_request)) }
-    });
-
-    let server = Server::bind(&addr).serve(make_svc);
-
-    println!("FluxStream server running on http://{}", addr);
-
-    if let Err(e) = server.await {
-        eprintln!("Server error: {}", e);
     }
 }
+
+fn main() -> std::io::Result<()> {
+    // Set up a TCP listener on port 7878
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
+    println!("Server listening on port 7878");
+
+    // Accept incoming connections in a loop
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection established!");
+
+                // Spawn a new thread to handle each client connection
+                thread::spawn(move || {
+                    handle_client(stream);
+                });
+            },
+            Err(e) => {
+                eprintln!("Failed to establish connection: {}", e);
+            }
+        }
+    }
+    Ok(())
+}
+
 
